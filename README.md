9d924daf970efd0b1f2ca6b58917a39b5bd8997b
Leon.Lin <lykpmh@gmail.com>
2020-08-30 12:09:33 +0800

    first push

---
+++ README.md
@@ -0,0 +1,34 @@
+# Bpmnv9
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.5.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## leon add
+0-1.npm uninstall -g @angular/cli (有安裝舊版要移除)
+0-2.npm cache clean --force (清除快取)
+1.npm install -g @angular/cli@9.1.5 (移除)
+2.將bpmn-js-custom 複製到node_modules
+3.run !
---
+++ src/app/diagram/diagram.component.ts
@@ -0,0 +1,304 @@
+import { Component, OnInit } from '@angular/core';
+import * as BpmnJS from 'bpmn-js/dist/bpmn-modeler.production.min.js';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+import propertiesPanelModule from 'bpmn-js/lib/Modeler';
+import propertiesProviderModule from 'bpmn-js/lib/Modeler';
+import { importDiagram } from './rx';
+import qaExtension from './qa.json';
+// import emptyJson from './empty.json';
+
+import customModule from 'bpmn-js-custom';
+import resizeTask from 'bpmn-js-task-resize/lib';
+
+import { throwError } from 'rxjs';
+import {
+  AfterContentInit,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  ViewChild,
+  SimpleChanges,
+  EventEmitter
+} from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+
+
+
+declare var jquery: any; // 這邊用 var
+declare let $: any; // 當然 let 也可以
+
+
+
+@Component({
+  selector: 'app-diagram',
+  templateUrl: './diagram.component.html',
+  styleUrls: ['./diagram.component.scss']
+})
+export class DiagramComponent implements AfterContentInit, OnChanges, OnDestroy {
+  private bpmnJS: BpmnJS;
+
+  @ViewChild('diagramDom') private el: ElementRef;
+  // tslint:disable-next-line: variable-name
+  @ViewChild('properties') private el_properties: ElementRef;
+
+  @ViewChild('inputXMLFile') private el_inputXMLFile: ElementRef;
+
+
+
+  @Output() private importDone: EventEmitter<any> = new EventEmitter();
+
+  @Input() private url: string;
+
+  // tslint:disable-next-line: variable-name
+  _bpmnModeler: BpmnModeler;
+
+  constructor(private http: HttpClient, private window: Window) {
+
+    // this.bpmnJS = new BpmnJS();
+
+    // this.bpmnJS.on('import.done', ({ error }) => {
+    //   if (!error) {
+    //     this.bpmnJS.get('canvas').zoom('fit-viewport');
+    //   }
+    // });
+
+    // this.window.applicationCache;
+    if (customModule.customRendererArg) {
+      customModule.customRendererArg.loadFileEvent = this.onLoadFileClicked.bind(this);
+      customModule.customRendererArg.saveFileEvent = this.onSaveFileClicked.bind(this);
+      customModule.customRendererArg.clearEvent = this.onClearClicked.bind(this);
+    }
+    console.log(customModule.customRendererArg);
+    // this.window.customPaletteArg = {};
+
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterViewInit() : void {
+    console.log('ngAfterViewInit');
+    console.log(this.el_inputXMLFile);
+    this._bpmnModeler = new BpmnModeler({
+      container: this.el.nativeElement,
+      additionalModules: [
+        // propertiesPanelModule,
+        // propertiesProviderModule
+        customModule,
+        resizeTask, // 要加 taskResizingEnabled: true
+      ],
+      moddleExtensions: {
+        qa: qaExtension
+      },
+      taskResizingEnabled: true,
+      propertiesPanel: {
+        // parent: '#properties'
+        parent: this.el_properties.nativeElement,
+      }
+    });
+    this.onClearClicked();
+  }
+
+  ngAfterContentInit(): void {
+    // this.bpmnJS.attachTo(this.el.nativeElement);
+
+
+    // console.log(this._bpmnModeler.importXML)
+    // this.loadUrl(this.url);
+    // this.loadUrl('assets/empty.bpmn');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // re-import whenever the url changes
+    if (this._bpmnModeler && changes.url) {
+      this.loadUrl(changes.url.currentValue);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.bpmnJS.destroy();
+  }
+
+  onLoadFileClicked() {
+    console.log('onLoadFileClicked');
+    this.el_inputXMLFile.nativeElement.click();
+  }
+
+  onSaveFileClicked() {
+    const self = this;
+    console.log('onSaveFileClicked');
+    if (this._bpmnModeler !== null) {
+      this._bpmnModeler.saveXML({ format: true },  (err, xml) => {
+        if (err) {
+          console.log(err);
+        } else {
+          self.writeContents(xml, 'save.xml', 'text/plain;charset=utf-8');
+        }
+      });
+    }
+  }
+
+  onClearClicked() {
+    console.log('onClearClicked');
+    const slef = this;
+    const path = 'assets/empty.bpmn';
+    // const path = 'assets/save_7.xml';
+    this.http.get(path, { responseType: 'text' }).subscribe(data => {
+      slef._bpmnModeler.importXML(data, (err, warnings) => {
+      });
+    });
+  }
+
+  writeContents(content, fileName, contentType) {
+    const a = document.createElement('a');
+    const file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+
+  handleFileInput(files: FileList) {
+    console.log(files);
+    if (files.length) {
+      this.loadXMLFile(files[0]);
+    }
+    // this.fileToUpload = files.item(0);
+  }
+
+  async loadXMLFile(file: File) {
+    const self = this;
+    if (file !== null) {
+      const fileReader = new FileReader();
+      const fileReaderPromise = new Promise(resolve => fileReader.onload = resolve);
+
+      fileReader.readAsBinaryString(file);
+
+      return fileReaderPromise.then(e => {
+        const text = fileReader.result;
+        console.log(text);
+        self.importXMLFromText(text);
+      });
+
+      // const reader = new FileReader();
+
+      // reader.onload = () => {
+      //   const text = reader.result;
+      //   console.log(text);
+      //   // self.importXMLFromText(text);
+      //   // importDiagram(self._bpmnModeler);
+      // };
+      // reader.onerror = () => {
+      //   console.log('load error');
+      // };
+      // reader.readAsText(file);
+
+    }
+  }
+
+  async importXMLFromText(text)  {
+
+    if (this._bpmnModeler !== null) {
+      this._bpmnModeler.importXML(text,  (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        this.setOverlay(this._bpmnModeler);
+        this._bpmnModeler.get('canvas').zoom('fit-viewport');
+
+      });
+    }
+  }
+
+  setOverlay(bpmnModeler: BpmnModeler) {
+    const overlays = bpmnModeler.get('overlays');
+    const elementRegistry = bpmnModeler.get('elementRegistry');
+    const taskName = 'ironman_0';
+    const shape = elementRegistry.get(taskName);
+    if (shape) {
+      const $overlayHtml =
+        $('<div class="highlight-overlay">')
+          .css({
+            width: shape.width,
+            height: shape.height
+          });
+
+      overlays.add(taskName, {
+        position: {
+          top: -0,
+          left: -0
+        },
+        html: $overlayHtml
+      });
+
+      overlays.add(taskName, {
+        position: {
+          bottom: 0,
+          right: 0
+        },
+        html: '<div class="diagram-note">leon 好棒棒?(我是overlays)</div>'
+      });
+    }
+  }
+
+  /**
+   * Load diagram from URL and emit completion event
+   */
+  loadUrl(url: string) {
+    const self = this;
+    return (
+      this.http.get(url, { responseType: 'text' }).pipe(
+        catchError(err => throwError(err)),
+        importDiagram(self._bpmnModeler )
+      ).subscribe(
+        (warnings) => {
+          this.importDone.emit({
+            type: 'success',
+            warnings
+          });
+        },
+        (err) => {
+          this.importDone.emit({
+            type: 'error',
+            error: err
+          });
+        }
+      )
+    );
+
+  //   return (
+  //     this.http.get(url, { responseType: 'text' }).pipe(
+  //       catchError(err => throwError(err)),
+  //       importDiagram(this.bpmnJS)
+  //     ).subscribe(
+  //       (warnings) => {
+  //         this.importDone.emit({
+  //           type: 'success',
+  //           warnings
+  //         });
+  //       },
+  //       (err) => {
+  //         this.importDone.emit({
+  //           type: 'error',
+  //           error: err
+  //         });
+  //       }
+  //     )
+  //   );
+  }
+}
+
+
+
+/**
+ * You may include a different variant of BpmnJS:
+ *
+ * bpmn-viewer  - displays BPMN diagrams without the ability
+ *                to navigate them
+ * bpmn-modeler - bootstraps a full-fledged BPMN editor
+ */
+
+
+
